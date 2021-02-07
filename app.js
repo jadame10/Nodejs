3b81ec71b9b8ef3e4be57a6c8680531293eba9ce
jadame10 <32987640+jadame10@users.noreply.github.com>
2021-02-07 14:48:22 +0100

    node js

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-const functions = require("./functions");
-functions.add(5,6);
+const express = require("express");
+const path = require("path");
+const port = 3000;
+
+const app = express();
+const functions = require("./functions.js");
+app.set("view engine","hbs");
+app.use('/assets', express.static(path.join(__dirname, "./assets")));
+app.use('/js', express.static(path.join(__dirname, "./js")));
+app.get("/", function (req,res){
+    res.render("index", {
+        title: "Zajęcia piatkowe",
+        subTitle: functions.subTitle
+    });
+})
+app.get("/about", function (req,res){
+    res.send("Strona o nas");
+})
+
+app.listen(port,(err)=>{
+    if (err) { return console.log(err)}
+    console.log(`Apka działa na porcie ${port}`)
+})
--- functions.js
+++ functions.js
@@ -1,6 +1,10 @@
 console.log("Hello World");
 const add = (a,b) => console.log(a+b);
+const subTitle = "WSB";
 
 module.exports ={
-    add
+    add,
+    subTitle
 }
+
+
